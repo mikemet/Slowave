ef82e3ed8aabd1197a31b0e128d65b523992ad07
mikemet <mikemet@outlook.com>
2021-02-15 20:01:47 +0300

    edit gulpfiile for output /public/index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 'use strict'
 
+const {
+    src,
+    dest
+} = require('gulp');
+
 function defaultTask(cb) {
     console.log('Hi, Gulp!');
     cb();
 }
 
+function htmlTask(cb) {
+    return src('src/*.html')
+        .pipe(dest('public/'));
+}
+
 exports.default = defaultTask;
+exports.html = htmlTask;
